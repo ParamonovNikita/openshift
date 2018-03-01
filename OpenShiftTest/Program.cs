38c82c086a5b893e70a40c54511b8c09c9fe73c5
Nikita_Paramonov <Nikita_Paramonov@epam.com>
2018-03-01 16:44:48 +0100

    Initial commit

---
+++ OpenShiftTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OpenShiftTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello OpenShift .net core!!!");
+            Console.ReadLine();
+        }
+    }
+}
